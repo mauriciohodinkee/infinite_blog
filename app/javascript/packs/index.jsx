44d1d0e5236cddce69441407616716656e1f2278
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-12 17:01:17 -0300

    lint(javascript): fix eslint issues

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-const Blog = props => (
-  <div>Blog {props.title}!</div>
-)
+const Blog = (props) => {
+  const { title } = props;
+  return (
+    <div>
+      Blog
+      {title}
+      !
+    </div>
+  );
+};
 
 Blog.defaultProps = {
-  title: 'Infinite Scrolling'
-}
+  title: 'Infinite Scrolling',
+};
 
 Blog.propTypes = {
-  title: PropTypes.string
-}
+  title: PropTypes.string,
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Blog title="InfiniteBlog by Mauricio Vieira" />,
     document.getElementById('root'),
-  )
-})
+  );
+});
