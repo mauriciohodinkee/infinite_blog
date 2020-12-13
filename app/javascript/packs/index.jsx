70438b36709ebea7372828694446821d8bb61d0e
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-13 19:40:57 -0300

    test(React): test Blog component rendering

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Blog = (props) => {
-  const { title } = props;
-  return (
-    <div>
-      Blog
-      {title}
-      !
-    </div>
-  );
-};
-
-Blog.defaultProps = {
-  title: 'Infinite Scrolling',
-};
-
-Blog.propTypes = {
-  title: PropTypes.string,
-};
+import Blog from './Blog';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
