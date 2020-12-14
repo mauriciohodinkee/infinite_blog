1b7443ce1a682052f6f18699a13035a4105c7f47
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-14 10:02:38 -0300

    feat(react): add footer

--- app/javascript/packs/Blog/index.jsx
+++ app/javascript/packs/Blog/index.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Header from './Header';
 import PropTypes from 'prop-types';
+import Header from './Header';
+import Footer from './Footer';
 
 const sections = [
   { title: 'Local posts', url: '#' },
   { title: 'Remote posts', url: '#' },
-]
+];
 
 const Blog = (props) => {
   const { title } = props;
@@ -15,13 +16,16 @@ const Blog = (props) => {
     <>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Blog" sections={sections} />
+        <Header title="Infinite Blog" sections={sections} />
         <main>
           Blog
           {title}
           !
         </main>
       </Container>
+      <Footer
+        description="Front-end based on Material-UI getting-started blog"
+      />
     </>
   );
 };
