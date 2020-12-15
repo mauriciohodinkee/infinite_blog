7c8b76f4c64ee704ce2fe3d143e7b62e162f8029
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-15 16:39:22 -0300

    feat: fetch local or remote posts

--- app/javascript/packs/Blog/Header.jsx
+++ app/javascript/packs/Blog/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Header(props) {
   const classes = useStyles();
-  const { sections, title } = props;
+  const { sections, title, setPostType } = props;
 
   return (
     <>
@@ -42,16 +42,14 @@ function Header(props) {
       </Toolbar>
       <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
         {sections.map((section) => (
-          <Link
+          <Button
             color="inherit"
-            noWrap
             key={section.title}
-            variant="body2"
-            href={section.url}
             className={classes.toolbarLink}
+            onClick={() => setPostType(section.postType)}
           >
             {section.title}
-          </Link>
+          </Button>
         ))}
       </Toolbar>
     </>
@@ -62,10 +60,11 @@ Header.propTypes = {
   sections: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
+      postType: PropTypes.string.isRequired,
     }),
   ).isRequired,
   title: PropTypes.string.isRequired,
+  setPostType: PropTypes.func.isRequired,
 };
 
 export default Header;
