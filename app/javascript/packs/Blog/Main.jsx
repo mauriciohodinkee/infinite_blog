3c820ca362a1582e57d988da5a430080fb058627
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-15 09:59:33 -0300

    feat: render blog posts as markdown

--- app/javascript/packs/Blog/Main.jsx
+++ app/javascript/packs/Blog/Main.jsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-// import Markdown from './Markdown';
+import Markdown from './Markdown';
 
-// const useStyles = makeStyles((theme) => ({
-//   markdown: {
-//     ...theme.typography.body2,
-//     padding: theme.spacing(3, 0),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  markdown: {
+    ...theme.typography.body2,
+    padding: theme.spacing(3, 0),
+  },
+}));
 
 function Main(props) {
-  // const classes = useStyles();
+  const classes = useStyles();
   const { posts, title } = props;
+  const formatToMarkdown = (post) => (`# ${post.id} ${post.title}
+
+  ${post.content}`);
 
   return (
     <Grid item xs={12} md={8}>
@@ -24,16 +27,16 @@ function Main(props) {
       </Typography>
       <Divider />
       {posts.map((post) => (
-        // <Markdown className={classes.markdown} key={post.substring(0, 40)}>
-        { post }
-        // </Markdown>
+        <Markdown className={classes.markdown} key={post.id}>
+          { formatToMarkdown(post) }
+        </Markdown>
       ))}
     </Grid>
   );
 }
 
 Main.propTypes = {
-  posts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
   title: PropTypes.string.isRequired,
 };
 
