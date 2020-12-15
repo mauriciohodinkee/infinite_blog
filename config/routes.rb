a10f581efd3ffd0683df62a664008d84f1023f23
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-15 15:40:24 -0300

    feat: add api/v1/local_posts endpoint with pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :local_posts, only: %w[index]
+    end
+  end
 end
