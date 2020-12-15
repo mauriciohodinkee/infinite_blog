50819e3b1c051e8cf3a9cce52720d1eb5fa4ec97
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-14 22:18:52 -0300

    feat(react): create Main component

--- app/javascript/packs/Blog/index.jsx
+++ app/javascript/packs/Blog/index.jsx
@@ -1,9 +1,24 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
+
 import Header from './Header';
 import Footer from './Footer';
+import Main from './Main';
+
+import PostService from '../services/post';
+
+const { getLocalPosts } = PostService;
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 const sections = [
   { title: 'Local posts', url: '#' },
@@ -11,16 +26,37 @@ const sections = [
 ];
 
 const Blog = (props) => {
-  const { title } = props;
+  const { title, type } = props;
+  const mainTitle = `${type} posts`;
+
+  const [errors, setErrors] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    await getLocalPosts()
+      .then((res) => setPosts(res.data))
+      .catch((error) => setErrors(error.response.data.messages));
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  });
+
+  if (errors) {
+    console.log(errors); // eslint-disable-line no-console
+  }
+
+  const classes = useStyles();
+
   return (
     <>
       <CssBaseline />
       <Container maxWidth="lg">
-        <Header title="Infinite Blog" sections={sections} />
+        <Header title={title} sections={sections} />
         <main>
-          Blog
-          {title}
-          !
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title={mainTitle} posts={posts} />
+          </Grid>
         </main>
       </Container>
       <Footer
@@ -32,10 +68,12 @@ const Blog = (props) => {
 
 Blog.defaultProps = {
   title: 'Infinite Scrolling',
+  type: 'Local',
 };
 
 Blog.propTypes = {
   title: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Blog;
