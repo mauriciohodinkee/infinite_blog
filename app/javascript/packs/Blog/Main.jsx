6ff37afacd70eee884df9690de2647f55dee8f68
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-16 14:24:48 -0300

    feat: fetch following pages

--- app/javascript/packs/Blog/Main.jsx
+++ app/javascript/packs/Blog/Main.jsx
@@ -5,17 +5,21 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Markdown from './Markdown';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   markdown: {
     ...theme.typography.body2,
     padding: theme.spacing(3, 0),
   },
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
 }));
 
 function Main(props) {
   const classes = useStyles();
-  const { posts, title } = props;
+  const { posts, title, currentPage, setPage } = props;
   const formatToMarkdown = (post) => (`# ${post.title}
 
   ${post.content}`);
@@ -28,9 +32,18 @@ function Main(props) {
       <Divider />
       {posts.map((post, index) => (
         <Markdown className={classes.markdown} key={index}>
-          { formatToMarkdown(post) }
+          { formatToMarkdown(post)}
         </Markdown>
       ))}
+      <Button
+        color="inherit"
+        key={1}
+        className={classes.toolbarLink}
+        onClick={() => setPage(() => currentPage + 1)}
+      >
+        Next
+      </Button>
+
     </Grid>
   );
 }
@@ -38,6 +51,8 @@ function Main(props) {
 Main.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object).isRequired,
   title: PropTypes.string.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  setPage: PropTypes.func.isRequired,
 };
 
 export default Main;
