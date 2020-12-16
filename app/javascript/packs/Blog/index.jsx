08bf62ab4e824c61801c449e6d7242a4325463cb
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-16 15:04:20 -0300

    feat: display errors

--- app/javascript/packs/Blog/index.jsx
+++ app/javascript/packs/Blog/index.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
-
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
@@ -18,6 +17,11 @@ const useStyles = makeStyles((theme) => ({
   mainGrid: {
     marginTop: theme.spacing(3),
   },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
 }));
 
 const sections = [
@@ -27,7 +31,6 @@ const sections = [
 
 const Blog = (props) => {
   const { title } = props;
-
   const [errors, setErrors] = useState();
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
@@ -44,7 +47,7 @@ const Blog = (props) => {
 
     await fetcher(page)
       .then((res) => setPosts(() => posts.concat(res.data)))
-      .catch((error) => setErrors(error.response.data.messages));
+      .catch((error) => setErrors(() => error.response.data.messages));
   };
 
   useEffect(() => {
@@ -65,6 +68,7 @@ const Blog = (props) => {
         <Header title={title} sections={sections} setPostType={setPostType} />
         <main>
           <Grid container spacing={5} className={classes.mainGrid}>
+            <Paper className={classes.paper}>{errors}</Paper>
             <Main title={mainTitle} posts={posts} currentPage={page} setPage={setPage} />
           </Grid>
         </main>
