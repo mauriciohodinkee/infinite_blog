19dd3f310f4f13f555ea5bb1c4203aabafda6eff
Mauricio Vieira <mauricio@mauriciovieira.net>
2020-12-16 15:10:23 -0300

    fix: run yarn format

--- app/javascript/packs/Blog/Main.jsx
+++ app/javascript/packs/Blog/Main.jsx
@@ -4,8 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import Markdown from './Markdown';
 import Button from '@material-ui/core/Button';
+import Markdown from './Markdown';
 
 const useStyles = makeStyles((theme) => ({
   markdown: {
